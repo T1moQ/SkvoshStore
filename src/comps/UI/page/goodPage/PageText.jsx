4b13272ffcfa47f7467db88d120251ea80a72c14
Timofey <pttima@yandex.ru>
2023-11-01 01:00:17 +0300

    fix

--- src/comps/UI/page/goodPage/PageText.jsx
+++ src/comps/UI/page/goodPage/PageText.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom'
 import DB from '../../../../DB/DB'
-import cl from '../goodPage/GoodPageMain.module.css'
+import cl from './GoodPageMain.module.css'
 
 const PageText = () => {
 
